b2e141579c60ea3e5db4d408260dbf3d81694dbd
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-22 10:29:27 -0500

    Update sign-in.php

---
+++ delegate/sign-in.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Delegate Sign in</title>
+    
+    <link rel="icon" href="../assets/images/icon.png">
+
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/sign-in.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+    <form class="form-signin">
+        <img class="mb-4" src="../assets/images/icon.png" alt="" width="72" height="72">
+        
+        <h1 class="h3 mb-3 font-weight-normal">Delegate Sign-in</h1>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <!-- <p></p> -->
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy; Felix Chen 2021</p>
+    </form>
+</body>
+</html>
