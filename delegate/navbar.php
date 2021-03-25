5e74b1b9cbaeac781c8c6ea1211e635e6e9fcc82
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-24 20:55:11 -0500

    Create navbar.php

---
+++ delegate/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><img src="../assets/images/icon.png" alt="" width="32" height="32"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="profile.php">Profile</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="sign-out.php">Sign out</a>
+            </li>
+            <!-- <li class="nav-item">
+            <a class="nav-link" href="profile.php">Profile</a>
+            </li> -->
+        </ul>
+    </div>
+</nav>
