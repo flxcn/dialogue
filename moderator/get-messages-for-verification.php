0bcf1df4162e92b2472924e7ca36e145536b3580
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-28 23:00:16 -0500

    Create get-messages-for-verification.php

---
+++ moderator/get-messages-for-verification.php
@@ -0,0 +1,45 @@
+<?php
+
+session_start(); // TODO: may or may not be necessary, if I'm posting the data
+
+if(!isset($_SESSION["moderator_signed_in"]) || $_SESSION["moderator_signed_in"] !== true){
+    header("location: sign-in.php");
+    exit;
+}
+
+require_once "../classes/ModeratorDashboard.php";
+
+$obj = new ModeratorDashboard();
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $committee_id = $_POST["committee_id"]; 
+    
+    $result = $obj->getMessagesByCommitteeId($committee_id);
+    
+    if($result) {
+
+        $messages = array();
+        foreach($result as $row) {
+            $created_on = date_create($row['created_on']);
+            $formatted_created_on = date_format($created_on, "D, M j\\t\h g:i A");
+            $messages[] = array(
+                "message_id" => $row['message_id'],
+                "message_content" => $row['message_content'],
+                "sender_representation" => $row['sender_representation'],
+                "receiver_representation" => $row['receiver_representation'],
+                "created_on" => $formatted_created_on
+            );
+        }
+		
+        // Send back data in JSON format
+        header('Content-Type: application/json');
+        echo json_encode($messages);
+        exit;
+    } 
+    else {
+        echo null;
+    }
+
+}
+?>
