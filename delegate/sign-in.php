989d8cb24fd1110ba74f274b4f276b80d703e3ea
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-30 12:23:36 -0500

    Update sign-in.php

--- delegate/sign-in.php
+++ delegate/sign-in.php
@@ -74,7 +74,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="icon" href="../assets/images/icon.png">
 
     <!-- Bootstrap core CSS -->
-    <!-- <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
