f93a8f3ff16b3c767a5ae06bf8d03831cdcc6d26
Felix <30987125+flxcn@users.noreply.github.com>
2021-03-30 12:23:30 -0500

    Update ModeratorDashboard.php

--- classes/ModeratorDashboard.php
+++ classes/ModeratorDashboard.php
@@ -9,19 +9,20 @@ class ModeratorDashboard {
 	}
 
     public function getMessagesByCommitteeId($committee_id) {
-        $sql = "SELECT      messages.message_id AS message_id,
-                            messages.message_content AS message_content,
-                            senders.representation AS sender_representation,
-                            receivers.representation AS receiver_representation,
-                            messages.created_on AS created_on
-                FROM        messages 
-                            INNER JOIN delegates AS senders
-                                ON messages.sender_id = senders.delegate_id
-                            INNER JOIN delegates AS receivers 
-                                ON messages.receiver_id = receivers.delegate_id
-                WHERE       messages.committee_id = :committee_id
-                            AND is_verified IS NULL
-                ORDER BY    created_on ASC LIMIT 4";
+        $sql = "SELECT  messages.message_id      AS message_id, 
+                        messages.message_content AS message_content, 
+                        senders.representation   AS sender_representation, 
+                        receivers.representation AS receiver_representation, 
+                        messages.created_on      AS created_on 
+                FROM    messages 
+                        INNER JOIN delegates AS senders 
+                                ON messages.sender_id = senders.delegate_id 
+                        INNER JOIN delegates AS receivers 
+                                ON messages.receiver_id = receivers.delegate_id 
+                WHERE   messages.committee_id = :committee_id 
+                        AND is_verified IS NULL 
+                ORDER   BY created_on ASC 
+                LIMIT   4 ";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(['committee_id' => $committee_id]);
